a08b40d115e0ee7cab6079a06abb957b16cba5fe
Cl30p4tr4 <Vanessa.Wehr@student.htw-berlin.de>
2025-07-18 00:11:50 +0200

    heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/components/Heading.tsx
 import React from 'react';
 
 const Heading = () => {
@@ -15,68 +14,3 @@ const Heading = () => {
 };
 
 export default Heading;
-
-
-const Heading2 = () => {
-    return (
-        <header className="bg-white-500 py-10 text-left">
-            <h3 className="text-2xl font-medium text-white-600 text-left">
-            Upload file
-            </h3>
-            <p className="text-base text-white-500 leading-relaxed">
-            Please upload your file in .pdf, .txt, .docx here. Click the button once it's uploaded and wait here. We'll analyse it and get back to you soon!
-            </p>
-        </header>
-    );
-};
-
-export default Heading;
-
-
-
-const Button = () => {
-    /*const handleClick = () => {
-        console.log('clicked');
-    }*/
-
-    return (
-        <div className="text-lg text-white-500 mt-2 text-left">
-        <button
-            //onCLick={handleClick}
-            className="bg-green-500 hover:bg-green-600 font-medium py-2 px-4 rounded">
-                Scan File
-        </button>
-        </div>
-    );
-};
-
-export default Button;
-
-
-
-/* import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-<<<<<<< HEAD
-        <h1>Test1</h1>
-=======
-       <h1>Test run</h1>
->>>>>>> 23bf068e158927ae719b50278628f184d75db292
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-<div class="text-2xl font-bold text-blue-600 p-4">👋 Preview build!</div>
