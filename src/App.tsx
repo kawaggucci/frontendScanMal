d4d72d850f9693f4a8c948c3df3046d4e9d5c043
Cl30p4tr4 <Vanessa.Wehr@student.htw-berlin.de>
2025-07-17 17:11:47 +0200

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,24 @@
-import { useState } from 'react'
+// src/components/Heading.tsx
+import React from 'react';
+
+const Heading = () => {
+  return (
+    <header className="bg-white py-10 text-center shadow-md">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Welcome to ScanMal
+      </h1>
+      <p className="text-lg text-gray-500 mt-2">
+        Your malware analysis made simple.
+      </p>
+    </header>
+  );
+};
+
+export default Heading;
+
+
+
+/* import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
