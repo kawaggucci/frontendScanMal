f42b9300f086186f8c56f879319d2c6b72dbd7ec
Cleo <149234359+Cl30p4tr4@users.noreply.github.com>
2025-07-18 13:07:23 +0200

    Update UploadHeading.tsx
    fine-tuning heading & changing colours

--- src/c-components/UploadHeading.tsx
+++ src/c-components/UploadHeading.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Heading = () => {
   return (
-    <header className="bg-white py-10 text-center shadow-md">
-      <h1 className="text-4xl font-bold text-gray-800">
-        Welcome to ScanMal
+    <header className="bg-white py-8 px-4 text-center shadow-md border-b">
+      <h1 className="text-4xl font-extrabold text-white-800 tracking-tight">
+        Welcome to <span className="text-white-800">ScanMal</span>
       </h1>
-      <p className="text-lg text-gray-500 mt-2">
+      <p className="text-lg text-green-100 mt-2">
         Your malware analysis made simple.
       </p>
     </header>
