eef2f34c86b44a52f4c91b3ed3cc21ee57937e66
Cleo <149234359+Cl30p4tr4@users.noreply.github.com>
2025-07-18 12:59:49 +0200

    Update App.tsx
    changed background colour to dark blue

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ReportButton from './c-components/ReportButton';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-blue-800">
       <Heading />
       <UploadInstructions />
       <ScanButton />
